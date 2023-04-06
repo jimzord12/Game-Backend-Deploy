1d604c34661b647ca77d1c1f5207ee52e95fe8d5
jimzord12 <jimzordgtx@gmail.com>
2023-04-06 16:42:14 +0300

    Added Correct CORS_6

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,17 +1,17 @@
-const allowedOrigins = require("./allowedOrigins");
+const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    const isAllowed = allowedOrigins.indexOf(origin) === -1 ? false : true;
-    console.log("This Origin is trying to access: ", origin);
-    if (isAllowed || !origin) {
-      console.log("Checking if the origin is allowed: ", isAllowed);
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  optionsSuccessStatus: 200,
-};
+    origin: (origin, callback) => {
+        console.log("This Origin is trying to access: ", origin);
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+        console.log("1 -> This Origin is trying to access: ", allowedOrigins.indexOf(origin) !== -1)
+        console.log("2 -> This Origin is trying to access: ", !origin)
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
 
 module.exports = corsOptions;
