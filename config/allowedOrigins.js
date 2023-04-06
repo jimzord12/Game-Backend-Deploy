e0034307c93f97700ee898c84ea68dc8b74f5517
jimzord12 <jimzordgtx@gmail.com>
2023-04-06 16:00:02 +0300

    Added Correct CORS_1

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
+  "https://642ebe9b0e7fec33f1c8658c--beautiful-malabi-7fdea3.netlify.app/",
   "https://www.yoursite.com",
-  https://642ebe9b0e7fec33f1c8658c--beautiful-malabi-7fdea3.netlify.app/
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://127.0.0.1:5173",
