c5bc0b0081c8754d7b8788871f6db486da40117a
jimzord12 <w.tool@yahoo.gr>
2024-01-14 18:42:23 +0200

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -16,6 +16,8 @@ const allowedOrigins = [
   "http://127.0.0.1:3001/",
   "http://localhost:3001",
   "http://localhost:3001/",
+  "http://localhost:3000",
+  "http://localhost:3000/",
 ];
 
 module.exports = allowedOrigins;
